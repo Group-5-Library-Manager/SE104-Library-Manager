b19754e937405517eae698e2eb3c72d049400917
manhtq0508 <manhtq0508@gmail.com>
2025-05-31 22:02:03 +0700

    Initialize database and DI (dependency injection)

---
+++ SE104-Library-Manager/Services/DatabaseService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SE104_Library_Manager.Data;
+
+namespace SE104_Library_Manager.Services;
+
+public class DatabaseService
+{
+    private DatabaseContext? _dbContext;
+
+    public DatabaseContext DbContext => GetDatabaseContext();
+
+    private DatabaseContext GetDatabaseContext()
+    {
+        if (_dbContext == null)
+        {
+            throw new InvalidOperationException("Database context is not initialized. Call Initialize first.");
+        }
+
+        return _dbContext;
+    }
+
+    public async Task Initialize(string connectionString)
+    {
+        var options = new DbContextOptionsBuilder<DatabaseContext>()
+            .UseSqlite(connectionString)
+            .Options;
+        
+        _dbContext = new DatabaseContext(options);
+
+        bool isNewlyCreated = await _dbContext.Database.EnsureCreatedAsync();
+        if (isNewlyCreated)
+        {
+            await EnsureDatabaseSeededAsync();
+        }
+    }
+
+    public async Task EnsureDatabaseSeededAsync()
+    {
+        DatabaseContext context = GetDatabaseContext();
+
+        if (!await context.DsQuyDinh.AnyAsync())
+        {
+            context.DsQuyDinh.Add(new Entities.QuyDinh());
+        }
+
+        await context.SaveChangesAsync();
+    }
+}

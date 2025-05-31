b3583c9a5973ae77535cfaf83462c47752446536
manhtq0508 <manhtq0508@gmail.com>
2025-05-31 21:59:21 +0700

    Create entities

---
+++ SE104-Library-Manager/Entities/TheLoai.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SE104_Library_Manager.Entities;
+
+public class TheLoai
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public required int MaTheLoai { get; set; }
+    public required string TenTheLoai { get; set; }
+
+    public ICollection<Sach> DsSach { get; set; } = new List<Sach>();
+}
